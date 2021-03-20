afb4d8910fe06729b384959ff259d80ab691b759
Elijah Hyndman <elijah@Elijahs-MacBook-Pro.local>
2021-03-19 22:19:11 -0400

    Created the WorldClock submodule for synchronizing in the Simulation Environment

---
+++ WorldClock/WorldClockTesting/WorldClockTest.java
@@ -0,0 +1,191 @@
+import WorldClock.WorldClock;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WorldClockTest {
+    WorldClock clk;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    @DisplayName("Clock calculates the correct wait periods from a given ratio and resolution in the constructor")
+    void testParameterizedConstructor() {
+        // The values of these updates and ratios have been hand calculated and hard coded to test
+        // that it gives the expected results
+        String predictedConfiguration;
+        String actualResult;
+        int resolution;
+        double ratio;
+
+        ratio = 1.0;
+
+        resolution = 1;
+        predictedConfiguration = generatePredictionString(1,1.0,1.0,1000);
+        clk = new WorldClock(resolution, ratio);
+        actualResult = clk.getConfiguration();
+        assert (clk.getConfiguration().equals(predictedConfiguration));
+
+        resolution = 2;
+        predictedConfiguration = generatePredictionString(2,1.0,2.0,500);
+        clk = new WorldClock(resolution, ratio);
+        actualResult = clk.getConfiguration();
+        assert (clk.getConfiguration().equals(predictedConfiguration));
+
+        resolution = 3;
+        predictedConfiguration = generatePredictionString(3,1.0,3.0,333);
+        clk = new WorldClock(resolution, ratio);
+        actualResult = clk.getConfiguration();
+        assert (clk.getConfiguration().equals(predictedConfiguration));
+
+        // New Ratio
+        ratio = 2.0;
+
+        resolution = 1;
+        predictedConfiguration = generatePredictionString(1,2.0,2.0,500);
+        clk = new WorldClock(resolution, ratio);
+        actualResult = clk.getConfiguration();
+        assert (clk.getConfiguration().equals(predictedConfiguration));
+
+        resolution = 2;
+        predictedConfiguration = generatePredictionString(2,2.0,4.0,250);
+        clk = new WorldClock(resolution, ratio);
+        actualResult = clk.getConfiguration();
+        assert (clk.getConfiguration().equals(predictedConfiguration));
+
+        resolution = 3;
+        predictedConfiguration =    generatePredictionString(3,2.0,6.0,166);
+        clk = new WorldClock(resolution, ratio);
+        actualResult = clk.getConfiguration();
+        assert (clk.getConfiguration().equals(predictedConfiguration));
+    }
+
+    @Test
+    @DisplayName("Clock calculates the correct wait periods from a given ratio or resolution from setters and default constructor")
+    void testSetterAndDefaultConstructor() {
+        String predictedConfiguration;
+        String actualResult;
+
+        // Check default constructor
+        clk = new WorldClock();
+        predictedConfiguration = generatePredictionString(10, 1.0, 10.0, 100);
+        actualResult = clk.getConfiguration();
+        assert (clk.getConfiguration().equals(predictedConfiguration));
+
+        // Note: MIN_ALLOWABLE_RATIO
+        clk.setRatio(0.001);
+        predictedConfiguration = generatePredictionString(10,0.0,0.0,100000);
+        actualResult = clk.getConfiguration();
+        assert (clk.getConfiguration().equals(predictedConfiguration));
+
+        clk.setRatio(2.0);
+        predictedConfiguration = generatePredictionString(10,2.0,20.0,50);
+        actualResult = clk.getConfiguration();
+        assert (clk.getConfiguration().equals(predictedConfiguration));
+
+        // Note: MAX_ALLOWABLE_RATIO
+        clk.setRatio(20.0);
+        predictedConfiguration = generatePredictionString(10,20.0,200.0,5);
+        actualResult = clk.getConfiguration();
+        assert (clk.getConfiguration().equals(predictedConfiguration));
+
+        clk.setRatio(3.0);
+        predictedConfiguration = generatePredictionString(10,3.0,30.0,33);
+        actualResult = clk.getConfiguration();
+        assert (clk.getConfiguration().equals(predictedConfiguration));
+
+        // Remember: Ratio was last configured to 3.0
+        clk.setResolution(2);
+        predictedConfiguration = generatePredictionString(2,3.0,6.0,166);
+        actualResult = clk.getConfiguration();
+        assert (clk.getConfiguration().equals(predictedConfiguration));
+
+        clk.setResolution(4);
+        predictedConfiguration = generatePredictionString(4,3.0,12.0,83);
+        actualResult = clk.getConfiguration();
+        assert (clk.getConfiguration().equals(predictedConfiguration));
+
+        // NOTE: Max allowable resolution
+        clk.setResolution(100);
+        predictedConfiguration = generatePredictionString(100,3.0,300.0,3);
+        actualResult = clk.getConfiguration();
+        assert (clk.getConfiguration().equals(predictedConfiguration));
+    }
+
+    @Test
+    @DisplayName("One measured Clock-Period correctly matches predicted single-period")
+    void correctTimePeriod() {
+        int measuredTime;
+        double acceptablePercentage = 0.1;
+
+        // periods of milliseconds to test
+        // We will hold resolution constant, adjust ratio as needed to achieve period
+        int[] periods = {3,5,20,30,100,160,200,1000};
+        int resolution = 20;
+        double ratio;
+
+        for (int period : periods) {
+            // Calculate appropriate period to yield desired period in milliseconds
+            ratio = 1000 / ( (double) period * (double) resolution);
+            clk = new WorldClock(resolution, ratio);
+            measuredTime = measureOnePeriod();
+            isAbout(measuredTime, clk.getMilliseconds(), acceptablePercentage);
+        }
+        //System.out.println("Total time waited: %d milliseconds".formatted(endTime-startTime));
+    }
+
+    public String generatePredictionString(int resolution, double ratio, double updates, int milliSec) {
+        /**
+         * Helper function for generating Hard-Coded results into the WorldClock Configuration string
+         */
+        String predictedConfiguration =    "resolution %d updates/WS\n".formatted(resolution) +
+                                            "ratio %.1f WS/RS \n".formatted(ratio) +
+                                            "%.1f updates per RS \n".formatted(updates)  +
+                                            "%d milliseconds per update".formatted(milliSec);
+        return predictedConfiguration;
+    }
+
+    public boolean isAbout(int reality, int expected,  double criterion) {
+        /**
+         * assesses whether reality value is acceptably close to expected.
+         *
+         * @param expected the value you calculated should occur in the measurement
+         * @param reality the actual value you measure from the measurement
+         * @param criterion the percent of the exepcted value which the reality value is allowed to vary above/below
+         *                  while still being considered acceptable
+         * Note: integer arithmetic will occur during errorBound calculation
+         */
+        int errorBound = (int) (criterion * expected);
+        int upperBound = expected + errorBound;
+        int lowerBound = expected - errorBound;
+        if (reality >= lowerBound && reality <= upperBound) {
+            System.out.println("%d is acceptably close to %d, within %.0f%% (+-%d)".formatted(reality, expected, criterion*100, errorBound));
+            return true;
+        } else {
+            System.out.println("%d is NOT acceptably close to %d, within %.0f%% (+-%d)".formatted(reality, expected, criterion*100, errorBound));
+            return false;
+        }
+    }
+
+    public int measureOnePeriod() {
+        /**
+         * measures the elapsed time in one period of the clk WorldClock object.
+         * @return int number of milliseconds that clk.once() has mesured to take
+         * @before clk must exist and be a valid WorldClock object, should be configured with desired ratio and resolution before
+         * @after clk has progressed one period
+         */
+        long startTime;
+        long measuredTime;
+
+        startTime = System.currentTimeMillis();
+        clk.once();
+        measuredTime = System.currentTimeMillis() - startTime;
+
+        return (int) measuredTime;
+    }
+}
---
+++ WorldClock/src/WorldClock/WorldClock.java
@@ -0,0 +1,185 @@
+package WorldClock;
+import java.util.concurrent.TimeUnit;
+
+public class WorldClock extends Thread {
+    /**
+     * Class for handling synchronization of Simulation-World time events (physics updates, etc).
+     * @author Elijah
+     *
+     * Definitions:
+     * - World Second: a single second of the Simulation World's Time
+     * - Real Second: a single second from the real world
+     */
+
+    /**
+     * Class Members
+     * @member resolution   number of updates per Simulation Second
+     * @member ratio        number of World Seconds per Real Second (A sped up simulation means more World Seconds per each Real Second)
+     * @member ticking      tracks whether the clock is ticking, used to halt clock from thread
+     *
+     * Derived Members
+     * updatesPerWorldSecond: number of updates per World Second to obtain appropriate physics resolution within simulation time
+     * updatesPerRealSecond: number of updates that must occur per an actual second, derived
+     */
+    private final int MAX_ALLOWABLE_RESOLUTION = 100;
+    private final double MAX_ALLOWABLE_RATIO = 20.000;
+    private final double MIN_ALLOWABLE_RATIO = 00.001;
+    private final int DEFAULT_RESOLUTION = 10;
+    private final double DEFAULT_RATIO = 01.000;
+    private final int QUICK_ADVANCE_PERIODS = 1000;
+
+    private int resolution = DEFAULT_RESOLUTION;
+    private double ratio = DEFAULT_RATIO;
+
+    private double updatesPerRealSecond;
+    private double realSecondsPerUpdate;
+    private int milliseconds;
+    private int microseconds;
+
+    private boolean ticking = false;
+
+
+    public WorldClock() {
+        // User has opted for default ratio and resolution
+        configure();
+    }
+
+
+    public WorldClock(int resolution, double ratio) {
+        // assert: default ratio will be used when setting resolution, then correct ratio will be used when setting ratio
+        //  (both resolution and ratio call configure() upon completion)
+        setResolution(resolution);
+        setRatio(ratio);
+    }
+
+
+    public void configure() {
+        /**
+         * recalculates configuration-parameters for how often to update, used when a parameter value has changed.
+         */
+        updatesPerRealSecond        = (double) resolution * ratio;              // U/WS * WS/RS = U/RS
+        realSecondsPerUpdate        = 1.0 / updatesPerRealSecond;               // 1/(U/RS) = RS/U
+        milliseconds                = (int) (1000 * realSecondsPerUpdate);      // RS/U * (1000 Milliseconds / 1 sec) = mRS/U
+        microseconds                = milliseconds * 1000 - 500;                // allows us to wait half a millisecond less than intended
+    }
+
+
+    public boolean setResolution(int resolution) {
+        /**
+         * Sets the resolution for the clock as physics-updates per Simulation-World Second.
+         * Shall filter for maximum resolution (MAX_ALLOWABLE_RESOLUTION) and minimum resolution (1)
+         *
+         * @param resolution number of physics-updates per each Simulation-World Second
+         * @return boolean operation successfully set to desired resolution / resolution was illegal, using closest default
+         */
+        if (resolution < MAX_ALLOWABLE_RESOLUTION) {
+            if (resolution > 0) {
+                // Set within appropriate resolution bounds
+                this.resolution = resolution;
+                configure();
+                return true;
+            } else {
+                this.resolution = 1;
+            }
+        } else {
+            this.resolution = MAX_ALLOWABLE_RESOLUTION;
+        }
+        // Attempted to set an illegal resolution
+        configure();
+        return false;
+    }
+
+
+    public boolean setRatio(double ratio) {
+        /**
+         * Sets the ratio of Simulation-World Seconds which will occur per each Real-World Second, a faster simulation speed
+         * means more Simulation-World Seconds per each Real-World Second.
+         * Shall filter for maximum speed (MAX_ALLOWABLE_RATIO) and minimum speed (MIN_ALLOWABLE_RATIO)
+         *
+         * @param ratio number of Simulation-World Seconds which occur per Real-World Second
+         * @return boolean operation successfully set to desired ratio / ratio was illegal, using closest default
+         */
+        if (ratio <= MAX_ALLOWABLE_RATIO) {
+            if (ratio >= MIN_ALLOWABLE_RATIO) {
+                // Set within appropriate ratio bounds
+                this.ratio = ratio;
+                configure();
+                return true;
+            } else {
+                this.ratio = MIN_ALLOWABLE_RATIO;
+            }
+        } else {
+            this.ratio = MAX_ALLOWABLE_RATIO;
+        }
+        // Attempted to set an illegal ratio
+        configure();
+        return false;
+    }
+
+
+    @Override
+    public void run() {
+        /**
+         * Runs the World Clock, calls appropriate update function on physics objects.
+         * Shall run on a new thread, so while(true) loop does not affect program execution
+         *
+         * @before time is not advancing, clock is still
+         * @after time is advancing, clock is moving, physics update-calls are broadcast
+         */
+        System.out.println("Clock has started ticking");
+        ticking = true;
+        while (ticking) {
+                // Traverse for period once per loop
+                once();
+        }
+    }
+
+
+    public void once() {
+        /**
+         * Traverses one period of clock-tick, useful for testing.
+         *
+         * @before nothing
+         * @after exactly one period ("milliseconds" milliseconds of real time) has been waited
+         */
+        try {
+            TimeUnit.MICROSECONDS.sleep(microseconds);
+            //TimeUnit.MILLISECONDS.sleep(milliseconds);
+            System.out.println("tick");
+        } catch (Exception e) {
+            System.out.println("Clock failed while advancing a single period");
+            e.printStackTrace();
+        }
+    }
+
+
+    public void halt() {
+        /**
+         * Halts the ticking of the clock.
+         * halt shall affect the while loop in run(), this function is called by the user who exists on another thread.
+         * The user updates the "ticking" member from their thread, which affects the loop execution on the clock's thread.
+         *
+         * @before clock may or may not be running.
+         * @after clock is definitely not ticking
+         */
+        ticking = false;
+        System.out.println("Clock has halted ticking");
+    }
+
+
+    public String getConfiguration() {
+        /**
+         * Generates debugging string about the current configurations of the World Clock attributes
+         */
+        String reportString =   "resolution %d updates/WS\n".formatted(resolution) +
+                                "ratio %.1f WS/RS \n".formatted(ratio) +
+                                "%.1f updates per RS \n".formatted(updatesPerRealSecond)  +
+                                "%d milliseconds per update".formatted(milliseconds) ;
+        return reportString;
+    }
+
+    public int getResolution() {return resolution;}
+    public double getRatio() {return ratio;}
+    public int getMilliseconds() {return milliseconds;}
+    public double getSeconds() {return realSecondsPerUpdate;}
+}
